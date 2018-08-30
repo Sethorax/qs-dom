7551fd1c58b10010753fee8d7777cb110a143f7a
Sethorax <info@sethorax.com>
2018-08-30 21:33:24 +0200

    style(tests): Formatted test files with prettier

--- test/utils.ts
+++ test/utils.ts
@@ -1,4 +1,4 @@
-export const triggerEvent = (eventName: string, target: Element|Document) => {
+export const triggerEvent = (eventName: string, target: Element | Document) => {
     const evt = document.createEvent("HTMLEvents");
     evt.initEvent(eventName, true, true);
     target.dispatchEvent(evt);
